f324433ed9b8bf10f1645abdff7ffb45b070f557
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-11 15:02:28 +0300

    Починил линии

--- TableTop2D/Core/WorkTable/SegmentCreator.cs
+++ TableTop2D/Core/WorkTable/SegmentCreator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using TableTop2D.Core.Base.Interfaces;
@@ -10,13 +11,13 @@ namespace TableTop2D.Core.WorkTable
     {
         private Canvas _SegmentCanvas;
         private ProjectTable _WorkTable;
-        
+
         #region Components of the Segment
 
         private FigureCreator _SegmentOrigin;
         private Line _SegmentLine;
         private FigureCreator _SegmentEnd;
-        
+
         #endregion
 
         public SegmentCreator(ref ProjectTable workTable, Brush color)
@@ -36,11 +37,14 @@ namespace TableTop2D.Core.WorkTable
             {
                 Stroke = color,
                 StrokeThickness = cellWidth < cellHeight ? cellHeight / 15 : cellWidth / 15,
+                X1 = pos.X,
+                Y1 = pos.Y,
+                X2 = pos.X,
+                Y2 = pos.Y
             };
 
-            Canvas.SetLeft(_SegmentLine, pos.X);
-            Canvas.SetTop(_SegmentLine, pos.Y);
             _SegmentCanvas.Children.Add(_SegmentLine);
+            workTable.TableCanvas.Children.Add(_SegmentCanvas);
 
             #endregion
 
@@ -53,20 +57,24 @@ namespace TableTop2D.Core.WorkTable
 
             _SegmentOrigin.FigureDeleted += SegmentDeleted;
             _SegmentEnd.FigureDeleted += SegmentDeleted;
-
-            workTable.TableCanvas.Children.Add(_SegmentCanvas);
         }
 
-        private void LinePrintOrigin()
-        {
-            var pos = _WorkTable.GetPointFigureInCell(_WorkTable.CursorPosition, 0, 0);
-            (_SegmentLine.X1, _SegmentLine.Y1) = (pos.X, pos.Y);
-        }
+        private void LinePrintOrigin()  =>
+            (_SegmentLine.X1, _SegmentLine.Y1) = (GetCurrentPoint().X, GetCurrentPoint().Y);
 
-        private void LinePrintEnd()
+        private void LinePrintEnd() =>
+            (_SegmentLine.X2, _SegmentLine.Y2) = (GetCurrentPoint().X, GetCurrentPoint().Y);
+
+        private Point GetCurrentPoint()
         {
-            var pos = _WorkTable.GetPointFigureInCell(_WorkTable.CursorPosition, 0, 0);
-            (_SegmentLine.X2, _SegmentLine.Y2) = (pos.X, pos.Y);
+            try
+            {
+                return _WorkTable.GetPointFigureInCell(Mouse.GetPosition(_SegmentCanvas), 0, 0);
+            }
+            catch
+            {
+                return Mouse.GetPosition(_SegmentCanvas);
+            }
         }
 
         private void SegmentDeleted()
