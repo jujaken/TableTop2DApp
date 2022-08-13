dbb960f684b7b5af71443c3ebf8c43b1acefa1a7
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-13 21:21:58 +0300

    Немного вид улучшил

--- TableTop2D/MainWindow.xaml.cs
+++ TableTop2D/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -182,6 +181,7 @@ namespace TableTop2D
                 button.Content = "⭕";
             }    
         }
+
         #endregion
     }
 }
